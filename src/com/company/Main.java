3035bd202e434c9b542b070cd3038eb714cd404a
eva.masakova <eva.masakova@post.cz>
2022-09-16 09:26:16 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("zadejte pocet kol: ");
+        int pocetopak = sc.nextInt();
+
+        scitani(0, 1, pocetopak);
+
+    }
+
+    static void scitani(int a, int b, int rounds) {
+
+        if (rounds == 0) return;
+        if (a == 0) System.out.print(a + " " + b);
+        int c = a + b;
+        System.out.print(" " + c);
+        a = b;
+        b = c;
+        rounds = rounds - 1;
+        scitani(a, b, rounds);
+
+    }
+}
